b7c3d120cde495ffe7cb9aa0b7f3dfcf1ff23e30
Alexander Grishkin <grinal82@gmail.com>
2023-07-05 23:41:05 +0300

    configs modified

--- src/js/main.js
+++ src/js/main.js
@@ -4,40 +4,39 @@ let isVisible = false;
 let popover = null;
 
 button.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (!isVisible) {
-        createPopover();
-    } else {
-        removePopover();
-    }
+  e.preventDefault();
+  if (!isVisible) {
+    createPopover();
+  } else {
+    removePopover();
+  }
 });
 
 function createPopover() {
-    popover = document.createElement("div");
-    popover.classList.add("popover");
-    popover.innerHTML = `
+  popover = document.createElement("div");
+  popover.classList.add("popover");
+  popover.innerHTML = `
     <div class="popover-title"><p>Popover Title</p></div>
     <div class="popover-content"><p>And here's some amazing content. It's very engaging. Right?</p></div>
   `;
-    container.appendChild(popover);
-    calculatePopoverPosition(); 
-    isVisible = true;
+  container.appendChild(popover);
+  calculatePopoverPosition();
+  isVisible = true;
 }
 
 function removePopover() {
-    popover.remove();
-    isVisible = false;
+  popover.remove();
+  isVisible = false;
 }
 
 function calculatePopoverPosition() {
-    const buttonRect = button.getBoundingClientRect();
-    const popoverWidth = popover.offsetWidth;
-    const popoverHeight = popover.offsetHeight;
-    console.log('popoverheight: ' + popoverHeight)
-    const popoverTop = buttonRect.top - popoverHeight - 10; // регулировка для отступа вверх от кнопки
-    const popoverLeft =
-        buttonRect.left + buttonRect.width / 2 - popoverWidth / 2;
+  const buttonRect = button.getBoundingClientRect();
+  const popoverWidth = popover.offsetWidth;
+  const popoverHeight = popover.offsetHeight;
+  console.log("popoverheight: " + popoverHeight);
+  const popoverTop = buttonRect.top - popoverHeight - 10; // регулировка для отступа вверх от кнопки
+  const popoverLeft = buttonRect.left + buttonRect.width / 2 - popoverWidth / 2;
 
-    popover.style.top = popoverTop + "px";
-    popover.style.left = popoverLeft + "px";
+  popover.style.top = popoverTop + "px";
+  popover.style.left = popoverLeft + "px";
 }
